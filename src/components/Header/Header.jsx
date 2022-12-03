b2a1f6c5b74b3b58a6450debd13697c4fa531166
Martazanov <saidmartazanov1@gmail.com>
2022-12-04 01:53:18 +0300

    week-project-front-header v.2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import styles from "./Header.module.scss"
+import React from "react";
+import styles from "./Header.module.scss";
 const Header = () => {
-    return (
-        <div className={styles.wrapper}>
-            <div className="container">
-                <div className={styles.content}>
-                    <img width={50} height={50} src={"assets/logo.png"} alt="logo" />
-                    <div className="adress">
-                        <img width={24} height={24} src="assets/location.png" alt="" />
-                         <input type="text"
-                                 placeholder='Введите адрес доставки' /> 
-                    </div>
-                    <div className="contact">
-                        <img width={24} height={24} src="assets/phone.png" alt="" />
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <img
+            className={styles.logo}
+            width={50}
+            height={50}
+            src={"assets/logo.png"}
+            alt="logo"
+          />
 
-                    </div>
-                  
-                </div>
+          <div className={styles.adress}>
+            <div className={styles.input}>
+              <img
+                className={styles.location}
+                width={16}
+                height={18}
+                src="assets/location.png"
+                alt="location"
+              />
+              <input
+                className={styles.search}
+                type={"search"}
+                placeholder="Введите адрес доставки . . ."
+              />
+              <img
+                width={18}
+                height={18}
+                src="assets/Search.svg"
+                alt="search"
+              />
             </div>
+            <div className={styles.contact}>
+              <div className={styles.call}>
+                <img
+                  width={16}
+                  height={16}
+                  src="assets/Calling.png"
+                  alt="calling"
+                />
+              </div>
+
+              <p>
+                Контакты: <br /> <span>+7 (928) 695-30-59</span>
+              </p>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Header;
