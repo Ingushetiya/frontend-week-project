b823cd14334a12726278af3736abefa4bc685fe5
Martazanov <saidmartazanov1@gmail.com>
2022-12-05 13:48:17 +0300

    делаю карточку корзины

--- src/components/Basket/index.jsx
+++ src/components/Basket/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import CartBasket from "../Cart-basket"
+import styles from "./Basket.module.scss"
 const Basket = () => {
     return (
-        <div>
-            <h1>Basket</h1>
+        <div className= {styles.wrapper}>
+            <div className={styles.title_basket}>
+                <span>&lt; К выбору блюда</span>
+                <div className={styles.basket_h1}>
+                    <div className={styles.line}></div>
+                   <p>КОРЗИНА</p> 
+                   <span>(в корзине {0} товара)</span>
+                </div>                              
+            </div>
+            <div className={styles.cartParrent}>
+                <CartBasket />
+            </div>
+            
         </div>
     );
 };
