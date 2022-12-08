1a5bbc9a9a6cf7cf841ae65ac4e9b3bddd6ba73a
Malsagov <rus.malsagov177@mail.ru>
2022-12-09 01:02:13 +0300

    Сделал скелетон для продуктов на главной странице

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,26 +1,37 @@
-import React, { useEffect } from 'react';
-import styles from "./Main.module.scss"
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchProduct } from '../app/features/BasketSlice';
-import Cart from '../Cart/index'
+import React, { useEffect } from "react";
+import styles from "./Main.module.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProduct } from "../app/features/BasketSlice";
+import Cart from "../Cart/index";
+import Skeleton from "../Skeleton";
 const Main = () => {
-    const product = useSelector((state)=> state.products.products)
-    const dispatch = useDispatch()
-    useEffect(()=>{
-        dispatch(fetchProduct())
-    }, [dispatch])
-    
-    return (
-        <ul className={styles.ul}>
-            {product.map(item=>{
-                return(
-                    <li key={item.id}><Cart 
-                        {...item}
-                    /></li>
-                )
-            })}
-        </ul>
-    );
+  const product = useSelector((state) => state.products.products);
+  const isLoading = useSelector((state) => state.products.loading);
+  console.log(isLoading);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchProduct());
+  }, [dispatch]);
+
+  return (
+    <ul className={styles.ul}>
+      {isLoading
+        ? [...new Array(6)].map((_, index) => {
+            return (
+              <li>
+                <Skeleton key={index} />
+              </li>
+            );
+          })
+        : product.map((item) => {
+            return (
+              <li key={item.id}>
+                <Cart {...item} />
+              </li>
+            );
+          })}
+    </ul>
+  );
 };
 
 export default Main;
