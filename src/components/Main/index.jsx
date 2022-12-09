ca2b247b0e3352961b3e0a53e581ee45bb545a77
Malsagov <rus.malsagov177@mail.ru>
2022-12-09 10:44:58 +0300

    Исправил ошибки

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,50 +1,19 @@
-
-import React, { useEffect } from "react";
-import styles from "./Main.module.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchProduct } from "../app/features/BasketSlice";
-import Cart from "../Cart/index";
-
-const Main = ({ index }) => {
-  
-  const product = useSelector((state) => state.products.products);
-  const filteredProduct = product.filter((item) => {
-    if (index === 0) {
-      return item;
-    }
-    return Number(item.categoryId) === index;
-  });
-
-  console.log(filteredProduct);
-  console.log(product);
-  console.log(index);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchProduct());
-  }, [dispatch]);
-
-  return (
-    <ul className={styles.ul}>
-      {filteredProduct.map((item) => {
-        return (
-          <li key={item.id}>
-            <Cart {...item} />
-          </li>
-        );
-      })}
-    </ul>
-  );
-
-
 import React, { useEffect } from "react";
 import styles from "./Main.module.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProduct } from "../app/features/BasketSlice";
 import Cart from "../Cart/index";
 import { SkeletonMain } from "../Skeleton";
-const Main = () => {
+
+const Main = ({ index }) => {
   const product = useSelector((state) => state.products.products);
   const isLoading = useSelector((state) => state.products.loading);
+  const filteredProduct = product.filter((item) => {
+    if (index === 0) {
+      return item;
+    }
+    return Number(item.categoryId) === index;
+  });
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -61,7 +30,7 @@ const Main = () => {
               </li>
             );
           })
-        : product.map((item) => {
+        : filteredProduct.map((item) => {
             return (
               <li key={item.id}>
                 <Cart {...item} />
